3592b5a06acaff1130cad145120e5688583da305
Tiago Amaro <tiagopadrela@gmail.com>
2021-09-16 13:43:36 -0300

    Setup session storage with an 1 hour expiration time

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module DeviseRememberableSessionTest
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.session_store :cookie_store, key: 'devise-rememberable-session-test-session', expires: 1.hour
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
